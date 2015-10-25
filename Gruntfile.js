aa19635173736c6bcb43b6779fe9d86bb52022d4
BV Satyaram <bvsatyaram@gmail.com>
2015-10-25 18:10:55 +0530

    Uglify JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,20 @@ module.exports = function(grunt) {
       },
       js: {
         files: ['js/src/*.coffee'],
-        tasks: ['coffee']
+        tasks: ['coffee', 'uglify']
       }
     },
     coffee: {
       compile: {
         files: {
           'js/features.js': 'js/src/features.coffee'
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'js/app.min.js': ['js/features.js']
         }
       }
     }
@@ -38,6 +45,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['sass', 'cssmin', 'coffee', 'watch']);
+  grunt.registerTask('default', ['sass', 'cssmin', 'coffee', 'uglify', 'watch']);
 };
